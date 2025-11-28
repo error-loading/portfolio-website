7b9d831883ce9b265b18cadbe0f95146c6da6856
Gurjas Dhillon <gsingh.ont@gmail.com>
2025-11-28 01:53:17 -0500

    project section skeleton

---
+++ src/components/ProjectSection.tsx
@@ -0,0 +1,26 @@
+import ProjectCard from "./ProjectCard";
+import { projects } from "../data/project";
+
+const ProjectSection = () => {
+    return (
+        <section id="projects" className="project">
+            <div className="container">
+                <h2 className="section-title project-title">Projects</h2>
+                <div className="project-cards">
+                    {projects.map((project, index) => (
+                        <ProjectCard 
+                            key={project.title}
+                            project={project}
+                            index={index}
+                        />
+                    ))}
+                </div>
+                <div className="project-more">
+                    <button className="project-button">Show Projects</button>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default ProjectSection;
