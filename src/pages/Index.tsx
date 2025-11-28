f2b11d3da64e73cf55a6b5eb0125932d6fd98a36
Gurjas Dhillon <gsingh.ont@gmail.com>
2025-11-27 22:11:47 -0500

    create index page skeleton

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import ExperienceSection from "../components/ExperienceSection";
+
+const Index = () => {
+    return (
+        <main className="main-content">
+            <ExperienceSection />
+        </main>
+    )
+}
+
+export default Index;
