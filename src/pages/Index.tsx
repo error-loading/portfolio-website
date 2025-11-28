37eff1baba0bbe75d1abf4e75465aba92dff9fac
Gurjas Dhillon <gsingh.ont@gmail.com>
2025-11-28 00:18:05 -0500

    updated index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,10 @@
+import AboutSection from "../components/AboutSection";
 import ExperienceSection from "../components/ExperienceSection";
 
 const Index = () => {
     return (
         <main className="main-content">
+            <AboutSection />
             <ExperienceSection />
         </main>
     )
